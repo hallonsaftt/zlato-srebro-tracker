b3c862dde398b90093b28cb029b1b00bd9c2b213
hallonsaftt <hallonsaftt@gmail.com>
2025-06-26 14:59:32 +0200

    Nove promen2e

--- main.js
+++ main.js
@@ -10,64 +10,103 @@ const silverButton = document.getElementById('srebro-dugme');
 // console.log("Radi");
 
 // APIj
-async function dohvatiCenuZlata() {
-    const myHeaders = new Headers();
-    myHeaders.append("x-access-token", "goldapi-1cngqsmcd698rz-io");
-    myHeaders.append("Content-Type", "application/json");
+// async function dohvatiCenuZlata() {
+//     const myHeaders = new Headers();
+//     myHeaders.append("x-access-token", "goldapi-1cngqsmcd698rz-io");
+//     myHeaders.append("Content-Type", "application/json");
 
-    const requestOptions = {
-        method: 'GET',           
-        headers: myHeaders,      
-        redirect: 'follow'      
-    };
+//     const requestOptions = {
+//         method: 'GET',           
+//         headers: myHeaders,      
+//         redirect: 'follow'      
+//     };
 
-    try {
-        const response = await fetch("https://www.goldapi.io/api/XAU/USD", requestOptions);
-        const data = await response.json();
+//     try {
+//         const response = await fetch("https://www.goldapi.io/api/XAU/USD", requestOptions);
+//         const data = await response.json();
         
-        console.log("API podaci:", data);
-        return data; 
+//         console.log("API podaci:", data);
+//         return data; 
+        
+//     } catch (error) {
+//         console.log("Greška:", error);
+//     }
+// }
+
+// async function dohvatiCenuSrebra() {
+//     const myHeaders = new Headers();
+//     myHeaders.append("x-access-token", "goldapi-1cngqsmcd698rz-io");
+//     myHeaders.append("Content-Type", "application/json");
+
+//     const requestOptions = {
+//         method: 'GET',           
+//         headers: myHeaders,      
+//         redirect: 'follow'      
+//     };
+
+//     try {
+//         const response = await fetch("https://www.goldapi.io/api/XAG/USD", requestOptions);
+//         const data = await response.json();
+        
+//         console.log("API podaci:", data);
+//         return data; 
+        
+//     } catch (error) {
+//         console.log("Greška:", error);
+//     }
+// }
+
+
+// dohvatiCenuZlata();
+// dohvatiCenuSrebra();
+
+
+//nova metoda
+
+async function dohvatiCeneMetala() {
+    const myHeaders = new Headers();
+    myHeaders.append("x-api-key", "sk_0Eeac16a8BE3B6D624f8A0D4E507f39D9A0D315Da4Cf687F");
+    
+    const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow"
+    };
+    
+    try {
+        const response = await fetch("https://metals.g.apised.com/v1/latest?symbols=XAU,XAG&base_currency=USD", requestOptions);
+        const data = await response.json();
+
+        const zlatoCena = data.data.rates.XAU;    
+        const srebroCena = data.data.rates.XAG;   
+        const valuta = data.data.base_currency;   
+        
+        // console.log("Svi podaci:", data);
+        // console.log("Zlato: ", zlatoCena, "Srebro", srebroCena, "valuta: ", valuta);
+        // return data;
+
+        return {
+        zlatoCena: zlatoCena,
+        srebroCena: srebroCena,
+        valuta: valuta
+        };
         
     } catch (error) {
         console.log("Greška:", error);
     }
 }
 
-async function dohvatiCenuSrebra() {
-    const myHeaders = new Headers();
-    myHeaders.append("x-access-token", "goldapi-1cngqsmcd698rz-io");
-    myHeaders.append("Content-Type", "application/json");
-
-    const requestOptions = {
-        method: 'GET',           
-        headers: myHeaders,      
-        redirect: 'follow'      
-    };
-
-    try {
-        const response = await fetch("https://www.goldapi.io/api/XAG/USD", requestOptions);
-        const data = await response.json();
-        
-        console.log("API podaci:", data);
-        return data; 
-        
-    } catch (error) {
-        console.log("Greška:", error);
-    }
-}
-
-
-dohvatiCenuZlata();
-dohvatiCenuSrebra();
+dohvatiCeneMetala();
 
 
 async function cenaZlata() {
 
     try {
-        const trenutniPodaci = await dohvatiCenuZlata();
-        const novaCena = trenutniPodaci.price; 
-        const staraCena = 2465; 
-        const valuta = trenutniPodaci.currency;
+        const trenutniPodaci = await dohvatiCeneMetala();
+        const novaCena = trenutniPodaci.zlatoCena; 
+        console.log(trenutniPodaci.zlatoCena)
+        const staraCena = 5465; 
+        const valuta = trenutniPodaci.valuta;
         
       
         if(novaCena > staraCena){
@@ -94,10 +133,10 @@ async function cenaSrebra() {
 
 
     try {
-        const trenutniPodaci = await dohvatiCenuSrebra();
-        const novaCena = trenutniPodaci.price; 
-        const staraCena = 2465; 
-        const valuta = trenutniPodaci.currency;
+        const trenutniPodaci = await dohvatiCeneMetala();
+        const novaCena = trenutniPodaci.srebroCena; 
+        const staraCena = 24.12; 
+        const valuta = trenutniPodaci.valuta;
         
         
         if(novaCena > staraCena){
